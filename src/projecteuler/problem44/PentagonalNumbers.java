ca88d385587c8793a216de2d7e97c4680531b398
james-norcross <james.norcross@gmail.com>
2015-02-18 20:03:44 -0600

    final commit

--- src/projecteuler/problem44/PentagonalNumbers.java
+++ src/projecteuler/problem44/PentagonalNumbers.java
@@ -9,78 +9,59 @@ public class PentagonalNumbers {
 	 * Date: 2/16/15
 	 * Purpose: Project Euler Problem 44
 	 * Description: Problem deals with pentagonal numbers defined by Pn = n(3n-1)/2.  The problem is to find a Pj and Pk 
-	 * such that Pj+Pk is a pentagonal number and |Pj-Pk| is also a pentagonal number with the final stipulation that 
+	 * such that Pj+Pk is a pentagonal number and Pj-Pk is also a pentagonal number with the final stipulation that 
 	 * |Pj-Pk| be minimized.
 	 */
 	public static void main(String[] args) {
 		
-		int dIndex, j, k, jmax, kmax;
-		long answer = 0;
+		int j, k, jmax = 1000000000;
+		long Pj, Pk, diffMax = 1000000000;
 		boolean done = false;
+		long start = System.currentTimeMillis();
 		
-		// note: don't know how many numbers in the pentagonal series will be needed to solve the problem.
-		// Could generate each number in the sequence as needed and use an array list to hold them, but
-		// for simplicity just generate 30000 elements in the series up front
-		long[] pNumbers = new long[30000];
-		pNumbers[0] = 0;		
-		for(long n = 1; n < 30000; n++) {
-			pNumbers[(int)n] = getPentagonalNumber(n);
-		}
-		
-		// since interested in Pj-Pk being pentagonal and minimized will step through pentagonal series
-		// starting from P1 trying to find a Pj and Pk that meet the conditions
-		dIndex = 1;
-		
-		while(!done) {
+		// step through j, k space. Max limit on j is determined by the current minimum difference.  It is set when the first
+		// possible solution is achieved below.  If any smaller differences are found jmax is reduced.  k counts down from
+		// j-1 to a minimum value which is either k = 1 or a value of k such that Pj-Pk is greater than diffMax.  diffMax is 
+		// also set when the first possible solution is found.  Subsequent smaller possible solutions would further reduce diffMax
+		for(j = 2; j < jmax; j++) {
 			
-			// first need to find maximum j, k that needs to be considered for this dIndex
-			j = dIndex + 1;
-			k = j + 1;
-			while(pNumbers[k] - pNumbers[j] <= pNumbers[dIndex]) {
-				j++;
-				k++;
-			}
-			kmax = k-1;
-			jmax = j-1;
-
+			Pj = (j*((3*j)-1))/2;
+			Pk = Pj;
 			
-			// now need to test all differences in valid range of j and k
-			for(j = dIndex + 1; j <= jmax && !done; j++) {
-
-				for(k = j+1; k <= kmax && !done; k++) {
-					//System.out.println(pNumbers[dIndex] + "   " + jmax + "   " + kmax + "   " + (pNumbers[k] - pNumbers[j]));
-					//System.out.println(j + "   " + k);
-
-					if((pNumbers[k] - pNumbers[j] == pNumbers[dIndex]) && isPentagonal(pNumbers[k] + pNumbers[j])) {
-						done = true;
-						answer = pNumbers[dIndex];
-					}
+			for(k = j-1; (k > 0) && (Pj - Pk < diffMax); k--) {
+				
+				Pk = (k*((3*k)-1))/2;
+				
+				if(isPentagonal(Pj - Pk) && isPentagonal(Pj + Pk)) {
+					
+					System.out.println("Possible solution");
+					System.out.println("Pj-Pk=" + (Pj - Pk) + "   j=" + j + "    k=" + k);
+					
+					// can limit loop on k now using this as minimum solution
+					diffMax = Pj - Pk;
+					
+					// find max j required to verify that solution is minimum for Pj - Pk.  Basically need to 
+					// check to point where Pj - Pj-1 > solution.  Analytically find that Pn - Pn+1 = 3n +1
+					int tempMax = (int)((Pj - Pk - 1)/3) + 1;
+					if (tempMax < jmax)
+						jmax = tempMax;
+										
 				}
 			}
-			dIndex ++;
 		}
 		
-		System.out.println("The answer is " + answer);
+		System.out.println("Time to complete " + (System.currentTimeMillis() - start) + " milliseconds");
 		
 	}
 	
-	private static long getPentagonalNumber(long n) {
-		long pN =(n*((3*n)-1))/2;
-		return pN;
-	}
 
+	// test for is pentagonal found on Wikipedia
 	private static boolean isPentagonal(long number) {
 		
-		long n = (int)Math.floor(Math.sqrt((2*number)/3));
-		long pN = getPentagonalNumber(n);
+		long n = (long)(((Math.sqrt((double)((24*number)+1)))+1)/6.0);
 		
-		while(pN <= number) {
-			if(pN == number)
-				return true;
-			else {
-				n++;
-				pN = getPentagonalNumber(n);
-			}
+		if (number == ((n * ((3*n) - 1))/2)) {
+			return true;
 		}
 		
 		return false;
